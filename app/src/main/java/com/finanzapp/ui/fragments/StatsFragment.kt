018edb31b33ebee79f4e1d5e7c973c6cf1f19557
Zencoal <programerzencoatl@gmail.com>
2025-04-16 21:30:43 -0600

    Estructura básica de la aplicación con navegación implementada

---
+++ app/src/main/java/com/finanzapp/ui/fragments/StatsFragment.kt
@@ -0,0 +1,18 @@
+package com.finanzapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.finanzapp.R
+
+class StatsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_stats, container, false)
+    }
+}
